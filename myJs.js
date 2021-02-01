41384f30d8a9be6f7a06b13b23f820468241a6cf
Dreeedy <dreeedyone@gmail.com>
2021-02-01 13:37:30 +0300

    типо работает

--- myJs.js
+++ myJs.js
@@ -1,10 +1,59 @@
-function get_hui()
-{
-    let hui = 'я тупой хуй';
-    return hui;
-}
-
+let i = 0;
 function punch()
 {
-    alert('ssss');
+    create_grid();
+
+    let main_button = document.getElementById('main_button');
+    main_button.textContent = 'ЕБАААААААААШ!';
+
+
+}
+
+function create_grid()
+{
+/*    document.body.style.background = 'red'; // сделать фон красным
+
+    setTimeout(() => document.body.style.background = '', 3000); // вернуть назад*/
+
+    let rectange = document.createElement('div');
+    rectange.id = 'punch'
+
+    rectange.style.position = 'absolute';
+    rectange.style.alignItems = 'normal';
+    rectange.style.height = '100px';
+    rectange.style.width = '100px';
+    rectange.style.background = 'red';
+    rectange.style.borderRadius = '50px';
+
+    rectange.style.textAlign = 'center';
+
+    rectange.onclick = function () { change_margin(rectange) };
+
+/*    rectange.style.marginTop = '-40%';// -40 40
+    rectange.style.marginLeft = '0%';// 1 94*/
+
+    let text = document.createElement('h2');
+    text.style.marginTop = '25%';
+    text.textContent = 'Пузо)';
+
+
+
+    rectange.append(text);
+
+    document.body.append(rectange);
+}
+function change_margin(myRectangle)
+{
+    myRectangle.style.marginTop = getRandomInt(-40, 40)+'%';// -40 40
+    myRectangle.style.marginLeft = getRandomInt(1, 94)+'%';// 1 94
+
+    let score = document.getElementById('score');
+    score.textContent = i;
+    i++;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
 }
